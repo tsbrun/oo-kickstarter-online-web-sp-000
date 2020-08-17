440046832caeab25c3bb358af2c07b2a814e36c9
Anya Brun <tsbrun@icloud.com>
2020-08-17 16:52:38 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,14 @@
+class Backer
+  attr_reader :name
+  attr_accessor :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    self.backed_projects << project
+    project.backers << self
+  end
+end
