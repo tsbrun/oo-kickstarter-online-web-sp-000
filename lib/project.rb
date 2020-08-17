af75896ac2dbb398013b616a2700f17df19f5a56
Anya Brun <tsbrun@icloud.com>
2020-08-17 16:35:42 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,13 @@
 class Project
   attr_reader :title
-  attr_accessor :backers 
+  attr_accessor :backers
 
   def initialize(title)
     @title = title
     @backers = []
+  end
+
+  def add_backer(backer)
+    self.backers << backer
   end
 end
